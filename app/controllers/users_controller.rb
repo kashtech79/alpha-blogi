47d4edd746cde871dc2c36a897131a5918eb0059
kashtech79 <kashifnizami@gmail.com>
2020-04-22 13:39:00 +0200

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,20 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Hi #{@user.username}, Your account has been created"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
     
